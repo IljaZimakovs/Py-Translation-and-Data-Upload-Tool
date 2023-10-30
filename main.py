c8c5f27f1d382a2dab03d6e050d6a86847ee3384
DevRex-0201 <dev.rex.0201@gmail.com>
2023-10-30 10:13:09 +0900

    add comment

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ import boto3
 import dotenv
 from google.cloud import translate_v2 as translate
 
+# Load environment variables from the .env file
 dotenv.load_dotenv()
 
+# Retrieve AWS and Google Cloud credentials from environment variables
 aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
 aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-
 region_name = os.getenv("REGION")
 table_name = os.getenv("TABLE")
 
-# Initialize the tkinter root window
+# Initialize the tkinter root window and hide it
 root = tk.Tk()
-root.withdraw()  # Hide the root window
+root.withdraw()
 
-# Ask the user to select a file through a file dialog
+# Ask the user to select a text file through a file dialog
 file_path = filedialog.askopenfilename(
     title="Select File", filetypes=(("Text files", "*.txt"), ("All files", "*.*"))
 )
@@ -29,10 +30,10 @@ if file_path:
     # Set Google Cloud credentials environment variable
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
 
-    # Initialize the translator
+    # Initialize the Google Cloud Translator client
     translator = translate.Client()
 
-    # Initialize DynamoDB client
+    # Initialize AWS DynamoDB client
     dynamodb = boto3.resource(
         "dynamodb",
         aws_access_key_id=aws_access_key_id,
@@ -47,6 +48,7 @@ if file_path:
     with open(file_path, encoding="utf-8") as file:
         text_data = file.read()
 
+    # Find and fix invalid list format in the text data
     invalide_list = re.findall(r"\'\[.*?\]\'", text_data)
     for item in invalide_list:
         new_str = item.replace("'", "")
@@ -54,20 +56,31 @@ if file_path:
         item_arr = [f"'{x}'" for x in item_arr]
         text_data = text_data.replace(item, "[" + ",".join(item_arr) + "]")
 
+    # Extract company details from the text data
     company_detail_list = re.findall(r"\{.*?\}", text_data)
-
     new_company_list = []
 
+    # Parse and evaluate JSON objects from the company details list
     for idx, item in enumerate(company_detail_list):
         if item[1] == '"':
             new_company_list.append(eval(item))
         else:
             new_company_list.append(eval(item))
 
+    # Extract domain names from the text data
     domain_list = text_data.split("\n")[::2]
 
-    # Define translate function
+    # Translate text using Google Cloud Translate API
     def translate_text(data):
+        """
+        Translate text from English to Japanese using Google Cloud Translate API.
+
+        Args:
+            data (str): Text to be translated.
+
+        Returns:
+            str: Translated text.
+        """
         if data:
             return translator.translate(
                 data, source_language="en", target_language="ja", format_="text"
@@ -75,14 +88,24 @@ if file_path:
         else:
             return ""
 
+    # Combine a list of strings into a comma-separated string
     def combine(data):
+        """
+        Combine a list of strings into a comma-separated string.
+
+        Args:
+            data (list): List of strings to be combined.
+
+        Returns:
+            str: Comma-separated string.
+        """
         if data:
             combined_data = ",".join(data)
             return combined_data
         else:
             return ""
 
-    # Upload the company data to Dynamo DB
+    # Upload company data to DynamoDB
     for i in range(0, len(new_company_list), 1):
         data_for_upload = {
             "info_type": "main",
